fe40117171183907613600bc3f020ebfcf1778f7
theTiv <justin.gluck@vibe.co.com>
2020-04-28 02:48:24 +0100

    Initial simple calculator dev

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Result from "./components/result";
+import Keypad from "./components/keypad";
 
-function App() {
+const App = (props) => {
+  const [result, setResult] = useState("");
+
+  const onClick = (btn) => {
+    if (btn === "=") {
+      calculation();
+    } else if (btn === "AC") {
+      cancel();
+    } else if (btn === "C") {
+      back();
+    } else {
+      setResult(result + btn);
+    }
+  };
+
+  const calculation = () => {
+    try {
+      setResult((eval(result) || "") + "");
+    } catch (e) {
+      setResult("ERROR");
+    }
+  };
+
+  const back = () => {
+    setResult(result.slice(0, -1));
+  };
+
+  const cancel = () => {
+    setResult("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="calculator__container">
+        <h1>Simple React Calculator</h1>
+        <Result result={result} />
+        <Keypad onClick={onClick} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/result.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const result = (props) => (
+  <div className="result">
+    <span>{props.result}</span>
+  </div>
+);
+
+export default result;
